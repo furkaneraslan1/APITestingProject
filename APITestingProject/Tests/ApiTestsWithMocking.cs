3597da91eb8915c7e47a2e8d007f2ba73883794a
Furkan Eraslan <eraslanfurkan1@gmail.com>
2024-10-02 14:07:43 +0200

    Add Mocking API Responses

---
+++ APITestingProject/Tests/ApiTestsWithMocking.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Moq;
+using RestSharp;
+using System.Net;
+using Xunit;
+
+namespace APITestingProject.Tests
+{
+    public class ApiTestsWithMocking
+    {
+        [Fact]
+        public async Task GetPosts_ShouldReturnMockedResponse()
+        {
+            // Arrange
+            var mockClient = new Mock<IRestClient>();
+            var mockRequest = new RestRequest("posts", Method.Get);
+
+            var mockResponse = new RestResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = "[{\"id\":1, \"title\":\"Mocked Post\", \"body\":\"This is a mocked response.\"}]"
+            };
+
+            mockClient.Setup(client => client.ExecuteAsync(It.IsAny<RestRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(mockResponse);
+
+            // Act
+            var response = await mockClient.Object.ExecuteAsync(mockRequest);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().Contain("Mocked Post");
+        }
+    }
+}
